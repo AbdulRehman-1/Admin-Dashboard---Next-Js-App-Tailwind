50d2a1e1310fd76a59cf053ac6581f8c4cb60f89
abdulrehman-11 <abdul.rehman.wb12@gmail.com>
2024-06-10 15:16:52 +0500

    updated the theme toggler

--- src/components/theme-toggler/index.tsx
+++ src/components/theme-toggler/index.tsx
@@ -1,18 +1,40 @@
 'use client';
-import { useEffect, useState } from 'react';
-import { useTheme } from 'next-themes';
+import { useState, useEffect } from 'react';
+
+import { MoonIcon, SunIcon } from '@heroicons/react/16/solid';
 
 const ThemeToggle = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+      document.querySelector('html')?.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      document.querySelector('html')?.setAttribute('data-theme', 'light');
+    }
+  }, [theme]);
+
   const handleTheme = () => {
-    document.documentElement.classList.add('dark');
-    document.querySelector('html')?.setAttribute('data-theme', 'dark');
-  };
-  const handleLight = () => {
-    document.documentElement.classList.remove('dark');
-    document.querySelector('html')?.setAttribute('data-theme', '');
+    setTheme('dark');
+    localStorage.setItem('theme', 'dark');
   };
 
-  return <div className='border border-gray'></div>;
+  const handleLight = () => {
+    setTheme('light');
+    localStorage.setItem('theme', 'light');
+  };
+
+  return (
+    <div>
+      {theme === 'dark' ? (
+        <SunIcon className='size-10' onClick={handleLight} />
+      ) : (
+        <MoonIcon className='size-10' onClick={handleTheme} />
+      )}
+    </div>
+  );
 };
 
 export default ThemeToggle;
